c8671241107d2f7154aa523673f7e19650d239d4
Manisha Gharti <ghartimaneesa@gmail.com>
2025-02-28 11:48:59 +0545

    Recursion in python

--- Basics/Recursion.py
+++ Basics/Recursion.py
@@ -1,40 +1,40 @@
 #Recurion
 #When a function call itself repeatedly
 
-# def show(n):
-#     if n == -1:
-#         return
-#     print(n)
-#     show(n-1)
-# show(9)
+def show(n):
+    if n == -1:
+        return
+    print(n)
+    show(n-1)
+show(9)
 
 
-# def show(n):
-#     if n == -1:
-#         return
-#     print(n)
-#     show(n-1)
-#     print("END")
-# show(9)
+def show(n):
+    if n == -1:
+        return
+    print(n)
+    show(n-1)
+    print("END")
+show(9)
 
 
-# def fact(n):
-#     if n == 0 or n==1 :
-#         return 1
-#     else:
-#         return fact(n-1) * n
-# num = int(input("Enter the number that you want to find the factorial of:"))
-# print(fact(num))
+def fact(n):
+    if n == 0 or n==1 :
+        return 1
+    else:
+        return fact(n-1) * n
+num = int(input("Enter the number that you want to find the factorial of:"))
+print(fact(num))
 
 #write a recursion number to calculate the sum of first n natural numbers
 
-# def sum_n(n):
-#     if n == 0:
-#         return 0
+def sum_n(n):
+    if n == 0:
+        return 0
   
-#     return sum_n(n-1) + n
-# intputs = int(input('Enter the numbers'))
-# print(sum_n(intputs))
+    return sum_n(n-1) + n
+intputs = int(input('Enter the numbers'))
+print(sum_n(intputs))
 
 
 #write the recursion function to print all elements in a list
